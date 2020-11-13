e0bf558c7b7f7a74f2de4642e012c104b03d6693
Munir Mahmud <munirmahmud6@gmail.com>
2020-11-13 10:01:08 +0600

    Toggling navigation for mobile done

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+/* SHOW MENU */
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId);
+
+        if(toggle && nav) {
+            toggle.addEventListener('click', () => {
+                nav.classList.toggle('show');
+            });
+        }
+};
+
+/* ACTIVE NAV LINK */
+const navLink = document.querySelectorAll('.nav__link');
+function linkAction() {
+    // Active link
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+
+    // Remove menu
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
